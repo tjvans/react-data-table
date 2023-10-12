ca0cb334c932cf5056c7d563f4d3a2a2361c9edb
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-12 11:12:23 +0200

    create producttable static mockup

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,11 +1,35 @@
 import ProductCatergoryRow from "./ProductCategoryRow";
 import ProductRow from "./ProductRow";
 
-export default function ProductTable () {
+export default function ProductTable ({products}) {
+    const rows = []
+    let lastCategory = null
+
+    products.forEach((product) => {
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCatergoryRow 
+                    category={product.category}
+                    key={product.category} />
+            )
+        }
+        rows.push(
+            <ProductRow 
+                product={product}
+                key={product.name} />
+        )
+        lastCategory = product.category
+    })
+
     return (
-        <>
-            <ProductCatergoryRow />
-            <ProductRow />
-        </>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
     )
 }
